2264dd34fcab5f1670938257dad073f6e7e3425a
emilioMarchi <em.98.nmynm@gmail.com>
2025-07-15 19:51:23 -0300

    commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="color-scheme" content="light"/>
+       
 
       </Head>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'false',
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
